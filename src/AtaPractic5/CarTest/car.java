dbca77c11ecd02d6bc073b73699acc4b0fdde171
Mehmet ata cebe <atacebe014@gmail.com>
2022-04-10 23:45:13 +0200

    Initial commit

---
+++ src/AtaPractic5/CarTest/car.java
@@ -0,0 +1,42 @@
+package AtaPractic5.CarTest;
+
+public class car {
+
+    public String brand,model;
+    public int year;
+    public double price;
+    public String color;
+    public int miles;
+
+    public car(String brand, String model, int year, double price, String color, int miles) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+        this.price = price;
+        this.color = color;
+        this.miles = miles;
+    }
+    public  void start(){
+        System.out.println(brand+" "+model+" is starting");
+    }
+    public void drive(){
+        System.out.println(brand+" "+model+" is driving");
+    }
+    /*
+    public void fly(){
+
+    }
+
+     */
+
+    public String toString() {
+        return "car{" +
+                "brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", year=" + year +
+                ", price=" + price +
+                ", color='" + color + '\'' +
+                ", miles=" + miles +
+                '}';
+    }
+}
