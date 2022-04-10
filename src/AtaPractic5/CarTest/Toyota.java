e9647eeea9e6378fa0d93652d7d73a02bfe91b20
Mehmet ata cebe <atacebe014@gmail.com>
2022-04-10 23:58:41 +0200

    add toyota specific feature

--- src/AtaPractic5/CarTest/Toyota.java
+++ src/AtaPractic5/CarTest/Toyota.java
@@ -6,4 +6,7 @@ public class Toyota extends car{
     public Toyota(String model, int year, double price, String color, int miles) {
         super("Toyota", model, year, price, color, miles);
     }
+    public void reliable(){
+        System.out.println(brand+" "+model+" is reliable");
+    }
 }
