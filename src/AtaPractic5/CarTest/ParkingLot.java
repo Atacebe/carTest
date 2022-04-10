be5664aeabc894f2b2592e8d5f35745d1c4bb6a0
Mehmet ata cebe <atacebe014@gmail.com>
2022-04-11 00:31:44 +0200

    add ParkingLot

---
+++ src/AtaPractic5/CarTest/ParkingLot.java
@@ -0,0 +1,11 @@
+package AtaPractic5.CarTest;
+
+public class ParkingLot {
+    public static void main(String[] args) {
+
+        Toyota toyota=new Toyota("Camry",2020,2000,"Gray",123456);
+        Tesla tesla=new Tesla("Model S",2022,94990,"red",0);
+        BMW bmw=new BMW("X5",2020,25000,"black",200);
+
+    }
+}
